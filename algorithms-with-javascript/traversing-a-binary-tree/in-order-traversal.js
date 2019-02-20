e464a4ceeb11f8fe455840a90855e93798dbf221
Thanh <thanhprofession@gmail.com>
2019-02-19 17:16:21 -0800

    initial commit

---
+++ algorithms-with-javascript/traversing-a-binary-tree/in-order-traversal.js
@@ -0,0 +1,60 @@
+function BinarySearchTree() {
+
+    function Node(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    this.root = null;
+
+    this.addNode = function(value) {
+        let node = new Node(value);
+        if (!this.root) {
+            this.root = node;
+        } else {
+            insertNode(this.root, node);
+        }
+    }
+
+    function insertNode(currentNode, newNode) {
+        if (newNode.value < currentNode.value) {
+            if (!currentNode.left) {
+                currentNode.left = newNode;
+            } else {
+                insertNode(currentNode.left, newNode);
+            }
+        } else {
+            if (!currentNode.right) {
+                currentNode.right = newNode;
+            } else {
+                insertNode(currentNode.right, newNode);
+            }
+        }
+    }
+
+    this.inOrderTraversal = function(callback) {
+        inOrderTraversalHelper(this.root, callback);
+    }
+
+    function inOrderTraversalHelper(node, callback) {
+        if (node) {
+            inOrderTraversalHelper(node.left, callback);
+            callback(node.value);
+            inOrderTraversalHelper(node.right, callback);
+        }
+    }
+
+}
+
+let tree = new BinarySearchTree();
+tree.addNode(3);
+tree.addNode(5);
+tree.addNode(14);
+tree.addNode(11);
+tree.addNode(25);
+tree.addNode(2);
+
+tree.inOrderTraversal(function log(value) {
+    console.log(value);
+});
